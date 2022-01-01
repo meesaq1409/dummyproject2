64b51fea98f514bd455c9b2acf147f45c5d0b827
meesaq khan <meesaq khan@DESKTOP-O9ONIV6>
2022-01-01 14:43:37 +0530

    meesaq

---
+++ src/test/java/project1dummy/CreateDummyTest.java
@@ -0,0 +1,34 @@
+package project1dummy;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class CreateDummyTest {
+
+	
+	@Test
+	
+	public void demo()
+	{	WebDriver driver = null;
+	
+		String browser=System.getProperty("browser");
+		String url = System.getProperty("url");
+	
+		if(browser.equals("chrome"))
+		{
+			driver=new ChromeDriver();
+			driver.get(url);	
+		}
+		
+		
+		System.out.println("hello there");
+	}
+	
+	
+	@Test(groups="regressionsuite")
+	public void execute()
+	{
+		System.out.println("vbcbg");
+	}
+}
---
+++ src/test/java/project1dummy/Dummytest.java
@@ -0,0 +1,20 @@
+package project1dummy;
+
+import org.testng.annotations.Test;
+
+public class Dummytest {
+
+	
+	@Test(groups="smokesuuite")
+	public void run()
+	{
+		System.out.println("fgdfgf");
+		
+	}
+	
+	@Test(groups="regressionsuite")
+	public void execute1()
+	{
+		System.out.println("grtjherui");
+	}
+}
